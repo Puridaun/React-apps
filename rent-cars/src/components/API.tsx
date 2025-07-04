6f88c0acce6fd5a06b0d85b387528ee2ce3cc94a
Hritcu Serafim <hritcuserafim01@gmail.com>
2025-07-04 15:09:07 +0300

    Small update

--- rent-cars/src/components/API.tsx
+++ rent-cars/src/components/API.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000/api/v1/rent-cars",
+  baseURL: "http://192.168.1.129:3000/api/v1/rent-cars",
   headers: {
     "Content-Type": "application/json",
   },
